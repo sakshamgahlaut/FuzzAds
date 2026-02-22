819d42a2d1f3c93f0c61e44535c257b125956bb0
Saksham Gahlaut <sakshamgahlaut111@gmail.com>
2026-02-22 22:02:52 +0530

    Updated API to production backend

--- frontend/admin-dashboard/src/pages/Login.jsx
+++ frontend/admin-dashboard/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://fuzzads.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- frontend/admin-dashboard/src/pages/Register.jsx
+++ frontend/admin-dashboard/src/pages/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://fuzzads.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
