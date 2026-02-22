8af4fe4e91633d87adcdccf35fd1e65f297c0d68
Saksham Gahlaut <sakshamgahlaut111@gmail.com>
2026-02-22 21:49:48 +0530

    Added root test route

--- backend/server.js
+++ backend/server.js
@@ -19,3 +19,7 @@ app.use("/api/auth", authRoutes);
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+
+app.get("/", (req, res) => {
+  res.send("FuzzAds Backend is Running 🚀");
+});
