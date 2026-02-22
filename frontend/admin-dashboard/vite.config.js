0c75b734b78a6635544cc3a9ce638d6edc51b13c
Saksham Gahlaut <sakshamgahlaut111@gmail.com>
2026-02-22 20:47:06 +0530

    Fixed base path for Vercel

--- frontend/admin-dashboard/vite.config.js
+++ frontend/admin-dashboard/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FuzzAds/",   // 👈 VERY IMPORTANT (repo name same hona chahiye)
-})
+  base: "/",   // ✅ Vercel ke liye always "/"
+});
